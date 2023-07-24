80e9ac98c764dd1f6c7118a0cb56257fa29e3513
SamsonVH <samson.vh.20@gmail.com>
2023-07-24 16:44:11 +0700

    Test

--- FBus_BE_Final/Controllers/TestController.cs
+++ FBus_BE_Final/Controllers/TestController.cs
@@ -1,71 +1,24 @@
-﻿using FBus_BE.DTOs;
-using FBus_BE.DTOs.InputDTOs;
-using FBus_BE.DTOs.ListingDTOs;
-using FBus_BE.DTOs.PageDTOs;
-using FBus_BE.Exceptions;
-using FBus_BE.Services;
-using Microsoft.AspNetCore.Authorization;
+﻿using FBus_BE.Services;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FBus_BE.Controllers
 {
-    [Route("api/Drivers")]
+    [Route("api/[controller]")]
     [ApiController]
-    public class DriversController2 : ControllerBase, IDefaultController<DriverPageRequest, DriverInputDto>
+    public class TestController : ControllerBase
     {
         private readonly IDriverService _driverService;
 
-        public DriversController2(IDriverService driverService)
+        public TestController(IDriverService driverService)
         {
             _driverService = driverService;
         }
 
-        [NonAction]
-        public Task<IActionResult> ChangeStatus([FromRoute] int id, [FromBody] string status)
-        {
-            throw new NotImplementedException();
-        }
-
-        [NonAction]
-        public Task<IActionResult> CreateWithBody([FromBody] DriverInputDto inputDto)
-        {
-            throw new NotImplementedException();
-        }
-
-        [NonAction]
-        public Task<IActionResult> CreateWithForm([FromForm] DriverInputDto inputDto)
-        {
-            throw new NotImplementedException();
-        }
-
-        [NonAction]
-        public Task<IActionResult> Delete([FromRoute] int id)
-        {
-            throw new NotImplementedException();
-        }
-
-        [NonAction]
-        public Task<IActionResult> GetDetails([FromRoute] int id)
-        {
-            throw new NotImplementedException();
-        }
         [HttpGet]
-        public async Task<IActionResult> GetList([FromQuery] DriverPageRequest pageRequest)
+        public IActionResult Test()
         {
-            return Ok("OK");
-        }
-
-        [NonAction]
-        public Task<IActionResult> UpdateWithBody([FromRoute] int id, [FromBody] DriverInputDto inputDto)
-        {
-            throw new NotImplementedException();
-        }
-
-        [NonAction]
-        public Task<IActionResult> UpdateWithForm([FromRoute] int id, [FromForm] DriverInputDto inputDto)
-        {
-            throw new NotImplementedException();
+            return Ok("Ok");
         }
     }
 }
