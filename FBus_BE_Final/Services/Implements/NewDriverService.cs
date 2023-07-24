013b491d724790e2592870fcc82f329102fe877c
SamsonVH <samson.vh.20@gmail.com>
2023-07-24 18:17:24 +0700

    Test

--- FBus_BE_Final/Services/Implements/NewDriverService.cs
+++ FBus_BE_Final/Services/Implements/NewDriverService.cs
@@ -12,9 +12,9 @@ namespace FBus_BE.Services.Implements
 {
     public class NewDriverService : INewDriverService
     {
-        //private Dictionary<string, string> errors;
-        //private readonly FbusMainContext _context;
-        //private readonly IMapper _mapper;
+        private Dictionary<string, string> errors;
+        private readonly FbusMainContext _context;
+        private readonly IMapper _mapper;
         //private readonly Dictionary<string, Expression<Func<Driver, object>>> _orderDict;
         //private readonly IFirebaseStorageService _storageService;
         //private const string cloudStoragePrefix = @"https://firebasestorage.googleapis.com/v0/b/fbus-388009.appspot.com/o/";
@@ -28,6 +28,12 @@ namespace FBus_BE.Services.Implements
         //    };
         //    _storageService = storageService;
         //}
+
+        public NewDriverService(FbusMainContext context, IMapper mapper) {
+            errors = new Dictionary<string, string>();
+            _context = context;
+            _mapper = mapper;
+        }
 
         public Task<bool> ChangeStatus(int id, string status)
         {
