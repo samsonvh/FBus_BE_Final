7d61f5379581a9bcf15fcad3baa93980d67547b6
SamsonVH <samson.vh.20@gmail.com>
2023-07-24 16:20:20 +0700

    Add Test

---
+++ FBus_BE_Final/Controllers/TestController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FBus_BE.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Test()
+        {
+            return Ok("Ok");
+        }
+    }
+}
