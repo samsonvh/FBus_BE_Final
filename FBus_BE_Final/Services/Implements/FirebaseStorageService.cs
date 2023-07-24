94753c1896e769bd4bcbe578e495ec8b683025cf
SamsonVH <samson.vh.20@gmail.com>
2023-07-24 18:55:47 +0700

    Test

--- FBus_BE_Final/Services/Implements/FirebaseStorageService.cs
+++ FBus_BE_Final/Services/Implements/FirebaseStorageService.cs
@@ -4,28 +4,28 @@ namespace FBus_BE.Services
 {
     public class FirebaseStorageService : IFirebaseStorageService
     {
-        private readonly StorageClient _storageClient;
+        //private readonly StorageClient _storageClient;
         private const string BucketName = "fbus-388009.appspot.com";
 
         public FirebaseStorageService()
         {
-            _storageClient = StorageClient.Create();
+            //_storageClient = StorageClient.Create();
         }
 
-        public async Task<Uri> UploadFile(string name, IFormFile file, string type)
-        {
-            Guid randomGuid = Guid.NewGuid();
-            using MemoryStream stream = new MemoryStream();
-            await file.CopyToAsync(stream);
-            var blob = await _storageClient.UploadObjectAsync(BucketName, type + "/" +
-                $"{name}-{randomGuid}", file.ContentType, stream);
-            var photoUri = new Uri(blob.MediaLink);
-            return photoUri;
-        }
+        //public async Task<Uri> UploadFile(string name, IFormFile file, string type)
+        //{
+        //    Guid randomGuid = Guid.NewGuid();
+        //    using MemoryStream stream = new MemoryStream();
+        //    await file.CopyToAsync(stream);
+        //    var blob = await _storageClient.UploadObjectAsync(BucketName, type + "/" +
+        //        $"{name}-{randomGuid}", file.ContentType, stream);
+        //    var photoUri = new Uri(blob.MediaLink);
+        //    return photoUri;
+        //}
 
-        public async Task DeleteFile(string name)
-        {
-            await _storageClient.DeleteObjectAsync(BucketName, name);
-        }
+        //public async Task DeleteFile(string name)
+        //{
+        //    await _storageClient.DeleteObjectAsync(BucketName, name);
+        //}
     }
 }
