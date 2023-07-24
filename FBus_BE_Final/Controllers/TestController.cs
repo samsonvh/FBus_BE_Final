3a74e309d288ad9e3e6a46e2d2696cafd3c0f4d8
SamsonVH <samson.vh.20@gmail.com>
2023-07-24 17:12:16 +0700

    Test

--- FBus_BE_Final/Controllers/TestController.cs
+++ FBus_BE_Final/Controllers/TestController.cs
@@ -10,7 +10,12 @@ namespace FBus_BE.Controllers
     [ApiController]
     public class TestController : ControllerBase, IDefaultController<DriverPageRequest, DriverInputDto>
     {
-        public TestController() { }
+        private readonly INewDriverService _driverService;
+
+        public TestController(INewDriverService driverService)
+        {
+            _driverService = driverService;
+        }
 
         [NonAction]
         public Task<IActionResult> ChangeStatus([FromRoute] int id, [FromBody] string status)
