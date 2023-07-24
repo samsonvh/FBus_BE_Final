d5a451f1e7a255ab8ad5ac635b5b1aeafb5a65a4
SamsonVH <samson.vh.20@gmail.com>
2023-07-24 16:26:04 +0700

    Test New Driver Controller

--- FBus_BE_Final/Controllers/DriversController.cs
+++ FBus_BE_Final/Controllers/DriversController.cs
@@ -10,7 +10,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace FBus_BE.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/Driversss")]
     [ApiController]
     public class DriversController : ControllerBase, IDefaultController<DriverPageRequest, DriverInputDto>
     {
--- FBus_BE_Final/Controllers/TestController.cs
+++ FBus_BE_Final/Controllers/TestController.cs
@@ -1,16 +1,120 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using FBus_BE.DTOs;
+using FBus_BE.DTOs.InputDTOs;
+using FBus_BE.DTOs.ListingDTOs;
+using FBus_BE.DTOs.PageDTOs;
+using FBus_BE.Exceptions;
+using FBus_BE.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FBus_BE.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/Drivers")]
     [ApiController]
-    public class TestController : ControllerBase
+    public class DriversController2 : ControllerBase, IDefaultController<DriverPageRequest, DriverInputDto>
     {
-        [HttpGet]
-        public IActionResult Test()
+        private readonly IDriverService _driverService;
+
+        public DriversController2(IDriverService driverService)
         {
+            _driverService = driverService;
+        }
+
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(bool))]
+        [Authorize("AdminOnly")]
+        [HttpPatch("{id:int}")]
+        public async Task<IActionResult> ChangeStatus([FromRoute] int id, [FromBody] string status)
+        {
+            try
+            {
+                return Ok(await _driverService.ChangeStatus(id, status));
+            }
+            catch (EntityNotFoundException entityNotFoundException)
+            {
+                return BadRequest(new ErrorDto { Title = "Entity Not Found", Errors = { { "message", entityNotFoundException.InforMessage } } });
+            }
+        }
+
+        [NonAction]
+        public Task<IActionResult> CreateWithBody([FromBody] DriverInputDto inputDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        [ProducesResponseType(StatusCodes.Status201Created, Type = typeof(DriverDto))]
+        [Authorize("AdminOnly")]
+        [HttpPost]
+        public async Task<IActionResult> CreateWithForm([FromForm] DriverInputDto inputDto)
+        {
+            try
+            {
+                int userId = Convert.ToInt32(User.FindFirst("Id").Value);
+                DriverDto? driver = await _driverService.Create(userId, inputDto);
+                return Ok(driver);
+            }
+            catch (DuplicateException duplicateException)
+            {
+                return BadRequest(new ErrorDto { Title = "Duplicated", Errors = duplicateException.GetErrors() });
+            }
+        }
+
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(bool))]
+        [Authorize("AdminOnly")]
+        [HttpDelete("{id:int}")]
+        public async Task<IActionResult> Delete([FromRoute] int id)
+        {
+            try
+            {
+                return Ok(await _driverService.Delete(id));
+            }
+            catch (EntityNotFoundException entityNotFoundException)
+            {
+                return BadRequest(new ErrorDto { Title = "Entity Not Found", Errors = new Dictionary<string, string>() { { "message", entityNotFoundException.InforMessage } } });
+            }
+        }
+
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(DriverDto))]
+        [Authorize("AdminOnly")]
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> GetDetails([FromRoute] int id)
+        {
+            return Ok(await _driverService.GetDetails(id));
+        }
+
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(DefaultPageResponse<DriverListingDto>))]
+        [HttpGet]
+        public async Task<IActionResult> GetList([FromQuery] DriverPageRequest pageRequest)
+        {
+            //return Ok(await _driverService.GetList(pageRequest));
             return Ok("Ok");
+        }
+
+        [NonAction]
+        public Task<IActionResult> UpdateWithBody([FromRoute] int id, [FromBody] DriverInputDto inputDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(DriverDto))]
+        [Authorize("AdminOnly")]
+        [HttpPut("{id:int}")]
+        public async Task<IActionResult> UpdateWithForm([FromRoute] int id, [FromForm] DriverInputDto inputDto)
+        {
+            try
+            {
+                int userId = Convert.ToInt32(User.FindFirst("Id").Value);
+                DriverDto? driver = await _driverService.Update(userId, inputDto, id);
+                return Ok(driver);
+            }
+            catch (DuplicateException duplicateException)
+            {
+                return BadRequest(new ErrorDto { Title = "Duplicated", Errors = duplicateException.GetErrors() });
+            }
+            catch (EntityNotFoundException entityNotFoundException)
+            {
+                return BadRequest(new ErrorDto { Title = "Entity Not Found", Errors = new Dictionary<string, string>() { { "message", entityNotFoundException.InforMessage } } });
+            }
         }
     }
 }
